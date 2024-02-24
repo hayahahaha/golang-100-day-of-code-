9d2e9da8ea2d1865bd2df0b0963015cd05b7c4da
duy.hoang <duy.hoang@smartr.co>
2024-02-24 23:02:09 +0700

    test database

--- rest/main.go
+++ rest/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	mode := os.Getenv("GIN_MODE")
+	gin.SetMode(mode)
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+
 	router := gin.Default()
 	router.GET("/", sayHello)
 
 	router.Run("localhost:3000")
+
 }
 
 func sayHello(c *gin.Context) {
+	name := os.Getenv("NAME")
+	fmt.Println("name in env %v", name)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Hello world!",
 	})
