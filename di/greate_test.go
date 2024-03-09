806e808f5da7fc1ec17a46eaa5f3ca2aa8e44e28
duy.hoang <duy.hoang@smartr.co>
2024-03-09 22:37:39 +0700

    di

--- di/greate_test.go
+++ di/greate_test.go
@@ -1,7 +1,19 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
-func TestGreate(t *testing.T) {
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Bob")
+
+	got := buffer.String()
+	want := "Hello Bob"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 
 }
